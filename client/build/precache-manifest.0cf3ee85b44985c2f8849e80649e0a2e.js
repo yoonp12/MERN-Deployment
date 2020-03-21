8a32890b077a81fbdced42d2498de4a9b016e6b8
Paul Yoon <yoonpaul12@gmail.com>
2020-03-20 18:47:03 -0700

    initial commit

---
+++ client/build/precache-manifest.0cf3ee85b44985c2f8849e80649e0a2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d30734f7347f854a5c0261556ea949d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8edc82d65756828d4014",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "641a0d3bc02c01805e4b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8edc82d65756828d4014",
+    "url": "/static/js/2.267e8581.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.267e8581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641a0d3bc02c01805e4b",
+    "url": "/static/js/main.6b14cb7e.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
